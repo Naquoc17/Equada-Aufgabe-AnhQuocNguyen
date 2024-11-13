15332cc2bfa84ad04a85fa9da500c2999adfa4c5
naquoc17 <naquoc17.konto@gmail.com>
2024-11-13 17:23:45 +0100

    die Loesung der zweiten Aufgabe

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-//Stufe 1 - leicht
+//Stufe 2 - mittel
+
+// Get the canvas element and context
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+// Initialize variables
 const square = {
    x: 40,
    y: 40,
    size: 20,
-   dx: 0,
-   dy: 0,
 };
 
-function moveSquare() {
-   square.x = (square.x + 20) % canvas.width;
+const img = new Image();
+img.src = "resource/images/snake-body.png";
+
+function drawImage() {
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   ctx.drawImage(img, square.x, square.y, square.size, square.size);
 }
+
+addEventListener("keydown", function (event) {
+   if (event.key === "ArrowRight") {
+      square.x += 10;
+      if (square.x > canvas.width - square.size) {
+         square.x = canvas.width - square.size;
+      }
+   } else if (event.key === "ArrowLeft") {
+      square.x -= 10;
+      if (square.x < 0) {
+         square.x = 0;
+      }
+   }
+});
 
 function gameLoop() {
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
-   moveSquare();
-   ctx.fillStyle = "red";
-   ctx.fillRect(square.x, square.y, square.size, square.size);
+   drawImage();
 }
 
-setInterval(gameLoop, 500);
+setInterval(gameLoop, 1000 / 120);
