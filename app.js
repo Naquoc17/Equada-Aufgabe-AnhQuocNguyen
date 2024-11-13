a0b8faad5877906445e0807606e209c902f63538
naquoc17 <naquoc17.konto@gmail.com>
2024-11-13 14:26:41 +0100

    Die Loesung der zweiten Aufgabe

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Stufe 1 - leicht
+//Stufe 2 - mittel
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -10,15 +10,51 @@ const square = {
    dy: 0,
 };
 
-function moveSquare() {
-   square.x = (square.x + 20) % canvas.width;
+const img = new Image();
+img.src = "resource/images/snake-body.png";
+
+let isMoving = true;
+
+function drawImage() {
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   ctx.drawImage(img, square.x, square.y, square.size, square.size);
 }
+
+function movingSnake() {
+   if (isMoving) {
+      square.x = (square.dx + square.x) % canvas.width;
+      square.y = (square.dy + square.y) % canvas.height;
+
+      if (square.x < 0) {
+         square.x = canvas.width - square.size;
+      }
+      if (square.y < 0) {
+         square.y = canvas.height - square.size;
+      }
+   }
+}
+
+addEventListener("keydown", function (event) {
+   if (event.key === "ArrowRight") {
+      square.dx = 20;
+      square.dy = 0;
+   } else if (event.key === "ArrowLeft") {
+      square.dx = -20;
+      square.dy = 0;
+   } else if (event.key === "ArrowUp") {
+      square.dy = -20;
+      square.dx = 0;
+   } else if (event.key === "ArrowDown") {
+      square.dy = 20;
+      square.dx = 0;
+   }
+});
 
 function gameLoop() {
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
-   moveSquare();
-   ctx.fillStyle = "red";
-   ctx.fillRect(square.x, square.y, square.size, square.size);
+   movingSnake();
+   drawImage();
 }
 
-setInterval(gameLoop, 500);
+img.onload = function () {
+   setInterval(gameLoop, 500);
+};
